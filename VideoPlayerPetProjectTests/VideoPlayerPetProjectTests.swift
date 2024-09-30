9e9a27e2e99d95178807a1c13dbdc3a08b881749
Антон <103773083+kantosev@users.noreply.github.com>
2024-09-30 21:58:55 +0300

    Initial commit

---
+++ VideoPlayerPetProjectTests/VideoPlayerPetProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  VideoPlayerPetProjectTests.swift
+//  VideoPlayerPetProjectTests
+//
+//  Created by Аркадий Варежкин on 30.09.2024.
+//
+
+import Testing
+@testable import VideoPlayerPetProject
+
+struct VideoPlayerPetProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
